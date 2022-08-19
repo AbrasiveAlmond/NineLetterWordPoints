49f63eba591134cb6ade2dc4371e725b0338a088
AltraMage <simonsenl2006@gmail.com>
2022-08-20 09:24:52 +1200

    Refactored CSS Sheet

--- checksite.js
+++ checksite.js
@@ -1,1 +1,6 @@
 console.log("You are currenly on the correct nine letter word website")
+document.getElementById("move_button").setAttribute(onclick, test());
+
+function test(){
+	console.log("Button CLiked")
+}
