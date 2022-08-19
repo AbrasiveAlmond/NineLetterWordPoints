e5e8f84cc4b269518263ba480012b29184d424f7
AltraMage <simonsenl2006@gmail.com>
2022-08-20 10:52:23 +1200

    Added a button to the page

--- background.js
+++ background.js
@@ -1,8 +1,6 @@
 //  Runs in the background on extention start 
-
-let color = '#3aa757';
-
+// TODO: Call Guesses varible from nineletterword.tompaton.com
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
+  chrome.storage.sync.set({guesses});
   console.log('Default background color set to %cgreen', `color: ${color}`);
 });
--- checksite.js
+++ checksite.js
@@ -1,6 +1,22 @@
 console.log("You are currenly on the correct nine letter word website")
-document.getElementById("move_button").setAttribute(onclick, test());
+var s = document.createElement('script');
+s.src = chrome.runtime.getURL('script.js');
+s.onload = function() {
+    this.remove();
+};
+(document.head || document.documentElement).appendChild(s);
 
-function test(){
-	console.log("Button CLiked")
+let btn = document.createElement("button");
+btn.innerHTML = "Check Points";
+btn.onclick = function () {
+  alert("Button is clicked");
+  var script = document.createElement('script');
+	script.textContent = 'console.log(GUESSES);';
+	(document.head||document.documentElement).appendChild(script);
+};
+document.body.appendChild(btn);
+
+function parseList(){
+	// The goal here is to get the guesses varible and 
 }
+
