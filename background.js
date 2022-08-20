09eb2545f3ee615aacb55b07650536b77255f57e
AbrasiveAlmond <64996167+AbrasiveAlmond@users.noreply.github.com>
2022-08-21 10:51:44 +1200

    Delete background.js

--- background.js
+++
@@ -1,6 +0,0 @@
-//  Runs in the background on extention start 
-// TODO: Call Guesses varible from nineletterword.tompaton.com
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({guesses});
-  console.log('Default background color set to %cgreen', `color: ${color}`);
-});
